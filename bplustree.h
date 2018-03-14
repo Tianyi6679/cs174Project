1e0692847722caf76e15759897e05d81871cf5d3
Tianyi Ma <timtianyima@gmail.com>
2018-03-14 14:44:33 -0700

    header file

---
+++ bplustree.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+cont int d = 21;
+
+struct node{
+  bool is_leaf;
+  int key_n
+  vector<string> key;
+  void* children[d+1];
+  void* parent;  
+  void initial();
+  bool insert(string key, vector<int> &values);
+  char* search(string key)
+};
+
